7a48e78d1fd1669532085286a4a5c5490a83b119
artix97 <80359636+artix97@users.noreply.github.com>
2022-04-15 14:50:15 +0200

    1.2
    improve scroll-spy navigation
    added photo, more page cards

--- js/program.js
+++ js/program.js
@@ -1,5 +1,15 @@
-function SendMailHandler() {
-    let name = document.getElementById("nameFormControl").value
-    alert("Hi " + name +"!"+" Your message has been sent")
-    
+function MailSuccess() {
+    let name = document.getElementById("nameFormControl").value;
+    alert("Hi " + name + "!");
+    document.getElementById("nameFormControl").value = "";
+    document.getElementById("mailFormControl").value = "";
+    document.getElementById("textFormControl").value = "";
+    var form = document.getElementById("contactForm");
+    function handleForm(event) { event.preventDefault(); } 
+    form.addEventListener('submit', handleForm);
+  
 }
+
+
+
+ 
